6cf71b3dd4beecae4b4bd4c8c45969276f8476de
24006768 <dakshadaksha2006@gmail.com>
2025-05-12 09:52:37 +0530

    success

---
+++ daksha/daksha/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for daksha project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daksha.settings')
+
+application = get_asgi_application()
